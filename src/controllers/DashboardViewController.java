0f0a3e4dd4912e048a71f62194947cf687430a88
Jaret Wright <hkkA223>
2021-04-01 14:11:31 -0400

    Added the ability to change from the dashboard view to the create student view

--- src/controllers/DashboardViewController.java
+++ src/controllers/DashboardViewController.java
@@ -1,13 +1,20 @@
 package controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.stage.Stage;
 import models.Professor;
 import models.Student;
 import utilities.DBUtility;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
@@ -43,5 +50,18 @@ public class DashboardViewController implements Initializable {
 
 
         professorsLabel.setText(String.format("Professors: %d",professorsListView.getItems().size()));
+    }
+
+    @FXML
+    private void createNewStudentButton(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("../views/createStudentView.fxml"));
+        Scene scene = new Scene(root);
+
+        //get the stage from the event that was passed in
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.setTitle("EdMuse - Create New Student");
+        stage.show();
     }
 }
