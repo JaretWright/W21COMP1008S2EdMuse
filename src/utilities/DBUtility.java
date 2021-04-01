579785d5715d6bca8028a72ff00e3de545558bd7
Jaret Wright <hkkA223>
2021-04-01 12:55:49 -0400

    Started to update the getStudentFromDB() method

--- src/utilities/DBUtility.java
+++ src/utilities/DBUtility.java
@@ -3,10 +3,14 @@ package utilities;
 import models.Professor;
 import models.Student;
 
+import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class DBUtility {
+    private static String user = "student";
+    private static String password = "student";
+    private static String connString = "jdbc:mysql://localhost:3306/edmuse";
 
     public static boolean validCRN(String crn)
     {
@@ -16,12 +20,35 @@ public class DBUtility {
     public static ArrayList<Student> getStudentsFromDB()
     {
         ArrayList<Student> students = new ArrayList<>();
+
+        //connect to the DB
+        Connection conn = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try{
+            //connect to the Database
+            conn = DriverManager.getConnection(connString, user, password);
+
+            statement = conn.createStatement();
+
+            //run the query on the DB
+            resultSet = statement.executeQuery("SELECT * FROM students");
+
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+
+        //query the DB for students
+        //loop over the result and create Student objects...add them to our students ArrayList
         students.add(new Student("Rose","Ruffner","3846 St. Paul StreetSt Catharines ON L2S 3A1", LocalDate.of(1975,8,27),100000001));
         students.add(new Student("Jack","Bradbury","867 rue des Églises Est Ste Cecile De Masham QC J0X 2W0", LocalDate.of(1979,10,14),100000002));
         students.add(new Student("Elanore","Sanders","1145 47th Avenue Grassland AB T0A 1V0", LocalDate.of(1940,9,25),100000003));
         students.add(new Student("Nancy","Walsh","1459 Harvest Moon Dr Unionville ON L3R 0L7", LocalDate.of(1999,1,12),100000004));
         students.add(new Student("Greta","Tolbert","642 Front Street Toronto ON M5J 2N1", LocalDate.of(1957,12,18),100000005));
         students.add(new Student("Barbara","Gable","3671 Scotchmere Dr Sarnia ON N7T 7T9", LocalDate.of(2002,11,13),100000006));
+
         return students;
     }
 
