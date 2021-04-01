f307d50055e4f5ebdc7fb063fd055633d69f7c00
Jaret Wright <hkkA223>
2021-04-01 14:22:04 -0400

    Added fx:id's to the createStudentView.fxml

--- src/controllers/CreateStudentViewController.java
+++ src/controllers/CreateStudentViewController.java
@@ -1,4 +1,35 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import models.Student;
+
+import java.time.LocalDate;
+
 public class CreateStudentViewController {
+
+    @FXML
+    private TextField firstNameTextField;
+
+    @FXML
+    private TextField lastNameTextField;
+
+    @FXML
+    private TextField addressTextField;
+
+    @FXML
+    private TextField studentNumberTextField;
+
+    @FXML
+    private DatePicker birthdayDatePicker;
+
+    @FXML
+    private void createStudent(){
+        Student newStudent = new Student("firstName",
+                                        "lastName",
+                                        "address",
+                                        LocalDate.of(2000,12,23),
+                                        1000002);
+    }
 }
